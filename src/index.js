2cb506cffe456b0c2ae4d54feff8723e9d100d04
Justin McCaleb <justinleemccaleb@gmail.com>
2020-05-27 16:24:14 -0500

    added properties to package.json

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ const $ = require('jquery');
 
 import {niceMessage} from "./say-hello";
 
-console.log(niceMessage);
+console.log(niceMessage('Justin'));
 
 let sayHello =(() => console.log('Hello'));
 
 sayHello();
 
-$('body').css('background-color', 'blue');
+$('body').css('background-color', 'lightgray');
